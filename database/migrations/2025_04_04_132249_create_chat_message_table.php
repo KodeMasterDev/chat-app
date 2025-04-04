36323adb7ed9e269cfa547011e4989fa699dd2a6
KodeMasterDev <sandrodevuza@gmail.com>
2025-04-04 10:47:02 -0300

    feat(database): add chat_messages table migration

--- database/migrations/2025_04_04_132249_create_chat_message_table.php
+++ database/migrations/2025_04_04_132249_create_chat_message_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chat_message', function (Blueprint $table) {
+        Schema::create('chat_messages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
